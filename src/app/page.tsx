b52d154d5c4477de291a0cdb37525b2a8723d058
Momchil Kalestrov <kalestrov.mo@gmail.com>
2024-12-16 11:05:58 +0200

    Commit with all the actual code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,78 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import React from 'react';
+import { NextPage } from 'next';
+import styles from './page.module.css';
+import { count } from 'console';
+import { start } from 'repl';
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+type cell = number | string;
+type segment = cell[][];
+type board = segment[];
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+const generateBoard = (): board => {
+    let board: board = [];
+    for (let i = 0; i < 4; i++) {
+        let column: segment = [];
+        
+        for (let j = 0; j < 5; j++) {
+            let row: cell[] = [];
+
+            for (let k = 0; k < 5; k++)
+                row.push(' ');
+
+            column.push(row);
+        };
+        board.push(column);
+    };
+    return board;
+};
+
+const Page: NextPage = () => {
+    /* a reducer would work best here */
+    const [ board, setBoard ] = React.useState<board>(generateBoard());
+
+    const generate = () => {
+        const flatten: Set<cell> = new Set(board.flat(4));
+
+        const getRandom = (start: number, end: number): number =>
+            Math.floor(Math.random() * (end - start)) + start;
+
+        let random = getRandom(0, 100);
+        while (flatten.has(random))
+            random = getRandom(0, 100);
+
+        setBoard((b) => {
+            const newBoard = b.map((column, i) =>
+                column.map((row, j) => 
+                    row.map((cell, k) => {
+                        if (random === i * 25 + j * 5 + k)
+                            return random;
+                        else return cell;
+                    })
+                )
+            );
+            return newBoard;
+        })
+    };
+
+    return (
+        <>
+            <div className={ styles.Board }>
+                { board.map((column, i) => (
+                    <div key={ i } className={ styles.Segments }>
+                        { column.map((row, j) => (
+                            <div key={ j } className={ styles.Row }>
+                                { row.map((cell, k) => (
+                                    <div key={ k }>{ cell }</div>
+                                )) }
+                            </div>
+                        )) }
+                    </div>
+                )) }
+            </div>
+            <button onClick={ generate }>Generate</button>
+        </>
+    );
+};
+
+export default Page;
